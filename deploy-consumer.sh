d0abdfa2071901fa0cfb27e128752742849062ea
Chris Powell <powellchristoph@gmail.com>
2015-12-01 16:03:49 -0500

    Add app json files.

--- deploy-consumer.sh
+++ deploy-consumer.sh
@@ -7,3 +7,5 @@ pip install pika
 pip install names
 
 curl https://raw.githubusercontent.com/powellchristoph/mesos-test/master/consumer.py > consumer.py
+
+python consumer.py
--- deploy-producer.sh
+++ deploy-producer.sh
@@ -6,3 +6,5 @@ source venv/bin/activate
 pip install pika
 
 curl https://raw.githubusercontent.com/powellchristoph/mesos-test/master/producer.py > producer.py
+
+python producer.py
